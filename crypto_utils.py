a00c80069fb7b0babb71c1c41a31188abe492203
Korban88 <korban88@gmail.com>
2025-07-01 14:24:59 +0300

    Update crypto_utils.py

--- crypto_utils.py
+++ crypto_utils.py
@@ -3,30 +3,56 @@ from pycoingecko import CoinGeckoAPI
 cg = CoinGeckoAPI()
 
 def get_top_ton_wallet_coins():
-    # Примерный список популярных монет, доступных в Telegram Wallet
+    # Примерный список монет из Telegram Wallet
     coin_ids = [
-        "bitcoin", "ethereum", "toncoin", "tether", "binancecoin", "usd-coin",
-        "polygon", "litecoin", "tron", "solana", "dogecoin", "avalanche-2"
+        "bitcoin", "ethereum", "toncoin", "tether", "binancecoin",
+        "usd-coin", "polygon", "litecoin", "tron", "solana",
+        "dogecoin", "avalanche-2", "aptos", "near", "the-open-network"
     ]
 
-    coins = cg.get_price(
+    # Получаем данные с CoinGecko
+    coins = cg.get_coins_markets(
+        vs_currency='usd',
         ids=coin_ids,
-        vs_currencies='usd',
-        include_24hr_change=True
+        order='market_cap_desc',
+        per_page=len(coin_ids),
+        price_change_percentage='24h,7d'
     )
 
-    # Выбираем монету с максимальным ростом
-    best = None
-    max_change = -999
+    best_coin = None
+    best_score = -999
 
-    for coin_id, data in coins.items():
-        change = data.get('usd_24h_change', 0)
-        if change > max_change:
-            max_change = change
-            best = {
-                'id': coin_id,
-                'price': data['usd'],
-                'change': round(change, 2)
+    for coin in coins:
+        price = coin['current_price']
+        volume = coin['total_volume']
+        change_24h = coin.get('price_change_percentage_24h_in_currency', 0)
+        change_7d = coin.get('price_change_percentage_7d_in_currency', 0)
+        name = coin['id']
+
+        # Простой скоринг
+        score = 0
+        if change_24h > 0:
+            score += 2
+        if change_7d > 0:
+            score += 1
+        if volume > 1_000_000:
+            score += 1
+        if change_24h > 3:
+            score += 1
+        if change_24h > 5:
+            score += 1
+        if change_24h < -1:  # признаки отката
+            score -= 2
+
+        if score > best_score:
+            best_score = score
+            best_coin = {
+                'id': name,
+                'price': round(price, 4),
+                'change_24h': round(change_24h, 2),
+                'change_7d': round(change_7d, 2),
+                'volume': int(volume),
+                'score': score
             }
 
-    return best
+    return best_coin
