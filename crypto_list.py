fcedaf3b7f7ff7ea5838b7135e7abfcba7c85733
Korban88 <korban88@gmail.com>
2025-07-04 23:18:43 +0300

    Create crypto_list.py

---
+++ crypto_list.py
@@ -0,0 +1,8 @@
+crypto_list = [
+    "storj", "bigtime", "uma", "anime", "ssv", "cvc", "bico", "iost", "waxp", "ygg", "bio",
+    "imx", "grt", "jto", "crv", "floki", "ens", "theta", "ldo", "jasmy", "iota", "btt",
+    "apt", "near", "internet-computer", "ethereum-classic", "ondo", "kaspa", "pol", "mnt",
+    "trump", "arbitrum", "render", "deep", "morpho", "starknet", "compound", "dydx", "rsr",
+    "conflux-network", "neo", "elrond", "mog-coin", "axie-infinity", "sats", "pha", "iq",
+    "memecoin", "aevo", "lisk", "dogs", "balancer", "illuvium", "scroll", "tensor"
+]
