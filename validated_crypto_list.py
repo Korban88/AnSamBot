991c0d00c99cacc8a0b496f7d343f1d100aeb0be
Korban88 <korban88@gmail.com>
2025-07-05 11:07:58 +0300

    Update validated_crypto_list.py

--- validated_crypto_list.py
+++ validated_crypto_list.py
@@ -1,8 +1,8 @@
 validated_crypto_list = [
-    "bitcoin", "ethereum", "tether", "toncoin", "binancecoin", "solana", "ripple", "cardano",
+    "bitcoin", "ethereum", "tether", "the-open-network", "binancecoin", "solana", "ripple", "cardano",
     "dogecoin", "avalanche-2", "polkadot", "chainlink", "litecoin", "matic-network",
     "stellar", "tron", "uniswap", "cosmos", "bitcoin-cash", "near", "aptos",
     "filecoin", "algorand", "the-graph", "aave", "eos", "decentraland", "tezos",
     "render-token", "axie-infinity", "gala", "immutable-x", "fantom", "flow",
-    "theta-token", "sushiswap", "chiliz", "pepe", "cake", "arbitrum", "blur", "injective"
+    "theta-token", "sushi", "chiliz", "pepe", "cake", "arbitrum", "blur", "injective-protocol"
 ]
