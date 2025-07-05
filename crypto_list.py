a21205cc2b2e17041ce346c0c6adca038b2689db
Korban88 <korban88@gmail.com>
2025-07-05 12:44:17 +0300

    Update crypto_list.py

--- crypto_list.py
+++ crypto_list.py
@@ -1,8 +1,11 @@
 crypto_list = [
-    "storj", "bigtime", "uma", "anime", "ssv", "cvc", "bico", "iost", "waxp", "ygg", "bio",
-    "imx", "grt", "jto", "crv", "floki", "ens", "theta", "ldo", "jasmy", "iota", "btt",
-    "apt", "near", "internet-computer", "ethereum-classic", "ondo", "kaspa", "pol", "mnt",
-    "trump", "arbitrum", "render", "deep", "morpho", "starknet", "compound", "dydx", "rsr",
-    "conflux-network", "neo", "elrond", "mog-coin", "axie-infinity", "sats", "pha", "iq",
-    "memecoin", "aevo", "lisk", "dogs", "balancer", "illuvium", "scroll", "tensor"
+    "storj", "uma", "anime", "ssv-network", "civic", "biconomy", "iost", "wax", "yield-guild-games",
+    "bio", "immutable-x", "the-graph", "jito-governance-token", "curve-dao-token", "floki",
+    "ethereum-name-service", "theta-token", "lido-dao", "jasmycoin", "miota", "bittorrent",
+    "aptos", "near", "internet-computer", "ethereum-classic", "ondo-finance", "kaspa",
+    "proof-of-liquidity", "mantle", "maga", "arbitrum", "render-token", "deepbrain-chain",
+    "morpho-network", "starknet", "compound-governance-token", "dydx", "reserve-rights-token",
+    "conflux-token", "neo", "multiversx", "mog-coin", "axie-infinity", "sats-ordinals",
+    "pha", "everipedia", "memecoin", "aevo", "lisk", "dogs-token", "balancer", "illuvium",
+    "scroll", "tensor"
 ]
