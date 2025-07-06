307b8257396ec089f8ab1147523285efbc3e72e4
Korban88 <korban88@gmail.com>
2025-07-06 21:22:44 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-# utils.py
-
-def escape_markdown(text):
-    """
-    Экранирует специальные символы для MarkdownV2 в Telegram.
-    """
-    escape_chars = r"\_*[]()~`>#+-=|{}.!"
-    return ''.join(f'\\{c}' if c in escape_chars else c for c in str(text))
