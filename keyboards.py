c736700daacfafb0274519132fcc6d5919c40039
Korban88 <korban88@gmail.com>
2025-07-06 21:23:51 +0300

    Delete keyboards.py

--- keyboards.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-def main_keyboard():
-    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(
-        KeyboardButton("Получить ещё сигнал"),
-        KeyboardButton("Остановить все отслеживания")
-    )
-    return keyboard
