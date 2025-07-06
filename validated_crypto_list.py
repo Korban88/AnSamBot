3e5394161203fce96b1a1b90e6bec8dd7e8220c8
Korban88 <korban88@gmail.com>
2025-07-06 21:22:36 +0300

    Delete validated_crypto_list.py

--- validated_crypto_list.py
+++
@@ -1,8 +0,0 @@
-validated_crypto_list = [
-    "bitcoin", "ethereum", "tether", "the-open-network", "binancecoin", "solana", "ripple", "cardano",
-    "dogecoin", "avalanche-2", "polkadot", "chainlink", "litecoin", "matic-network",
-    "stellar", "tron", "uniswap", "cosmos", "bitcoin-cash", "near", "aptos",
-    "filecoin", "algorand", "the-graph", "aave", "eos", "decentraland", "tezos",
-    "render-token", "axie-infinity", "gala", "immutable-x", "fantom", "flow",
-    "theta-token", "sushi", "chiliz", "pepe", "cake", "arbitrum", "blur", "injective-protocol"
-]
