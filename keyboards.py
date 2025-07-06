5b536483a3d87f4caf0dd2f898a934c792159e14
Korban88 <korban88@gmail.com>
2025-07-06 19:49:13 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,10 +1,9 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-def get_main_keyboard():
+def main_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.add(
         KeyboardButton("Получить ещё сигнал"),
-        KeyboardButton("Остановить все отслеживания"),
-        KeyboardButton("Старт")
+        KeyboardButton("Остановить все отслеживания")
     )
     return keyboard
