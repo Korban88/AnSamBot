88a730663198dd02ea1d8f028b6d0ea73b71aaf4
Korban88 <korban88@gmail.com>
2025-07-07 15:02:10 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,14 +1,8 @@
 from aiogram import Dispatcher, types
-from core.database import Database
-
-db = Database()
 
 def register_handlers(dp: Dispatcher):
     @dp.message_handler(commands=['start'])
-    async def start_cmd(message: types.Message):
-        await message.answer("Welcome to AnSam Crypto Bot!")
-    
-    @dp.message_handler(text="Get Signal")
-    async def get_signal(message: types.Message):
-        # Логика обработки
-        pass
+    async def cmd_start(message: types.Message):
+        await message.answer("Добро пожаловать в AnSam Bot!")
+
+    # Добавьте другие обработчики по аналогии
