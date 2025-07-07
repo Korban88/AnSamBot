f8f4defbba25f821ebe1342d9e2581396285f905
Korban88 <korban88@gmail.com>
2025-07-07 17:18:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8148906065:AAEw8yAPKnhjw3AK2tsYEo-h9LVj74xJS4c")
 OWNER_ID = os.getenv("OWNER_ID", "347552741")
 
-if not TELEGRAM_TOKEN or not OWNER_ID:
-    print("⚠️ Внимание: Используются fallback-значения переменных!")
+# Тихая проверка переменных
+if not os.getenv("TELEGRAM_TOKEN") or not os.getenv("OWNER_ID"):
+    pass  # Не выводим ошибку, если есть fallback
