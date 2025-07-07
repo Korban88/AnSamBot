24bdd99e19a904ded42d55b80ba67a9f0be598b6
Korban88 <korban88@gmail.com>
2025-07-07 18:46:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-# Production Config (v2.1.1)
+# Production Config (v2.1.2)
 TELEGRAM_TOKEN = "8148906065:AAEw8yAPKnhjw3AK2tsYEo-h9LVj74xJS4c"
 OWNER_ID = 347552741
 DB_ACTIVE = True
 
 def get_config():
     return {
-        'version': '2.1.1',
+        'version': '2.1.2',
         'mode': 'production',
+        'token': TELEGRAM_TOKEN,  # Добавлено это поле
         'owner': OWNER_ID,
         'db_status': DB_ACTIVE
     }
