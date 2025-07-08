bb08cab8bf3d9620237a004b5c677ba81ab40bbf
Korban88 <korban88@gmail.com>
2025-07-08 20:03:51 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8148906065:AAEw8yAPKnhjw3AK2tsYEo-h9LVj74xJS4c")
-    OWNER_ID = 347552741
-    COINGECKO_API_KEY = os.getenv("COINGECKO_API_KEY", "ваш_ключ")
-    BINANCE_API_KEY = os.getenv("BINANCE_API_KEY", "")  # Для будущей интеграции
-
-config = Config()
