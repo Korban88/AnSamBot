543ff062624c46bbfedcf4396291650feab1b0d9
Korban88 <korban88@gmail.com>
2025-07-08 16:18:23 +0300

    Delete handlers.py

--- handlers.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Dispatcher, types
-
-def register_handlers(dp: Dispatcher):
-    @dp.message_handler(commands=['start'])
-    async def cmd_start(message: types.Message):
-        await message.answer("Добро пожаловать в AnSam Bot!")
-
-    # Добавьте другие обработчики по аналогии
