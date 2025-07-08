ed84df30dee6bfb032169714764e9e4626cd1b4a
Korban88 <korban88@gmail.com>
2025-07-08 17:06:48 +0300

    Update analysis.py

--- core/analysis.py
+++ core/analysis.py
@@ -1,10 +1,6 @@
-from core.api.coingecko import fetch_coin_data
-
-def analyze_coin(coin_data):
-    conditions = [
-        coin_data['rsi'] < 45,
-        coin_data['ma50'] > coin_data['ma200'],
-        -6 <= coin_data['24h_change'] <= 0,
-        coin_data['volume_change_24h'] > 10
-    ]
-    return all(conditions)
+def analyze_coin(data):
+    return (
+        data['rsi'] < 45 and
+        data['price'] > data['ma200'] and
+        -6 <= data['24h_change'] <= 0
+    )
