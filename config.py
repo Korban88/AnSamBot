99e732ad99c9933e2923bfc9373bfa72964d65bc
Korban88 <korban88@gmail.com>
2025-07-08 20:05:41 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# config.py
+
+TELEGRAM_BOT_TOKEN = '8148906065:AAEw8yAPKnhjw3AK2tsYEo-h9LVj74xJS4c'  # токен твоего бота
+OWNER_ID = 347552741  # твой Telegram ID
+
+# Настройки сигнала
+TARGET_PROFIT_PERCENT = 5.0
+TRACKING_ALERT_PERCENT = 3.5
+MAX_PRICE_DROP_24H = -3.0
+MIN_GROWTH_PROBABILITY = 65.0
+
+# Путь к кешу
+INDICATORS_CACHE_FILE = "indicators_cache.json"
+TOP3_CACHE_FILE = "top3_cache.json"
