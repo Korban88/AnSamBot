fdffa94d775a2f90bed4980d346bd98a2b1f58ff
Korban88 <korban88@gmail.com>
2025-07-08 16:18:42 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-def get_config():
-    return {
-        'version': '2.1.3',
-        'mode': 'production',
-        'token': "8148906065:AAEw8yAPKnhjw3AK2tsYEo-h9LVj74xJS4c",
-        'owner': 347552741,
-        'db_status': True
-    }
