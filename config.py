a257bfc5fddcece2cc4ab2e0bc40b0d35a19e47d
Korban88 <korban88@gmail.com>
2025-07-10 15:14:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ OWNER_ID = 347552741  # твой Telegram ID
 TARGET_PROFIT_PERCENT = 5.0
 TRACKING_ALERT_PERCENT = 3.5
 MAX_PRICE_DROP_24H = -7.0           # раньше было -3.0
-MIN_GROWTH_PROBABILITY = 50.0       # раньше было 65.0
+MIN_GROWTH_PROBABILITY = 25.0       # раньше было 65.0
 
 # Путь к кешу
 INDICATORS_CACHE_FILE = "indicators_cache.json"
