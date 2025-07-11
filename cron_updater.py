9f7a16af460b85c516772e61a78e9c15829b168d
Korban88 <korban88@gmail.com>
2025-07-11 10:57:13 +0300

    Update cron_updater.py

--- cron_updater.py
+++ cron_updater.py
@@ -1,20 +1,11 @@
 # cron_updater.py
-import asyncio
-import logging
+
 import time
 from crypto_utils import fetch_and_cache_indicators
 
-logging.basicConfig(level=logging.INFO)
-
-async def cron_updater():
-    while True:
-        logging.info("Запуск фонового обновления индикаторов...")
-        fetch_and_cache_indicators()
-        logging.info("Ожидание до следующего обновления...")
-        await asyncio.sleep(1800)  # 30 минут
-
 if __name__ == "__main__":
-    try:
-        asyncio.run(cron_updater())
-    except KeyboardInterrupt:
-        print("Остановлено пользователем")
+    while True:
+        print("✅ Обновление indicators_cache.json запущено...")
+        fetch_and_cache_indicators()
+        print("✅ Ожидание 30 минут до следующего обновления...")
+        time.sleep(1800)  # 30 минут
