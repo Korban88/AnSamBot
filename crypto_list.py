4e1dd737ec67f1ad9ad23cb7355f521c890afd65
Korban88 <korban88@gmail.com>
2025-07-11 18:01:09 +0300

    Update crypto_list.py

--- crypto_list.py
+++ crypto_list.py
@@ -2,8 +2,8 @@ TELEGRAM_WALLET_CRYPTOS = [
     "bitcoin", "ethereum", "toncoin", "tether", "usd-coin", "binancecoin", "the-open-network",
     "dogecoin", "tron", "litecoin", "matic-network", "wrapped-bitcoin", "uniswap", "chainlink",
     "bitcoin-cash", "stellar", "filecoin", "arbitrum", "apecoin", "internet-computer", "true-usd",
-    "optimism", "render-token", "the-graph", "fantom", "axie-infinity", "gala", "osmosis", "mina-protocol",
-    "zilliqa", "aave", "pancakeswap-token", "chiliz", "1inch", "curve-dao-token", "compound-governance-token",
-    "balancer", "yearn-finance", "frax-share", "ankr", "iota", "kava", "waves", "terra-luna", "neo", "celo",
-    "qtum", "ontology", "icon", "harmony", "band-protocol", "sushi"
+    "optimism", "render-token", "the-graph", "fantom", "axie-infinity", "gala", "osmosis",
+    "mina-protocol", "zilliqa", "aave", "pancakeswap-token", "chiliz", "1inch", "curve-dao-token",
+    "compound-governance-token", "balancer", "yearn-finance", "frax-share", "ankr", "iota", "kava",
+    "waves", "terra-luna", "neo", "celo", "qtum", "ontology", "icon", "harmony", "band-protocol", "sushi"
 ]
