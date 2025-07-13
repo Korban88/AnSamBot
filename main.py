7fc351200bde804d466d25321bb908f71640fe14
Korban88 <korban88@gmail.com>
2025-07-13 13:31:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import logging
-from telegram import Update
-from telegram.ext import (Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters)
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 
-# Импорты твоих модулей:
 from config import TELEGRAM_BOT_TOKEN
 from handlers import start_handler, get_signal_handler, follow_coin_handler, stop_tracking_handler
 
@@ -16,9 +14,9 @@ def setup_application() -> Application:
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start_handler))
-    application.add_handler(MessageHandler(filters.Regex("Получить сигнал"), get_signal_handler))
+    application.add_handler(CallbackQueryHandler(get_signal_handler, pattern="^get_signal$"))
     application.add_handler(CallbackQueryHandler(follow_coin_handler, pattern="^follow_"))
-    application.add_handler(MessageHandler(filters.Regex("Остановить все отслеживания"), stop_tracking_handler))
+    application.add_handler(CallbackQueryHandler(stop_tracking_handler, pattern="^stop_tracking$"))
 
     return application
 
