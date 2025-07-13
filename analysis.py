0ec698257b1bf73719755ccaa120be0127750bd6
Korban88 <korban88@gmail.com>
2025-07-13 18:52:58 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import json
 import os
 import time
 from crypto_list import CRYPTO_LIST
+from crypto_utils import get_change_24h
 
 CACHE_FILE = "top_signals_cache.json"
 CACHE_TTL = 12 * 60 * 60  # 12 часов в секундах
@@ -34,10 +35,9 @@ async def get_top_signals():
 
     new_signals = []
     for coin in CRYPTO_LIST[:3]:
-        # Пока используем фиктивные значения change_24h и rsi
-        fake_change_24h = 2.5
-        fake_rsi = 55
-        probability = calculate_probability(fake_change_24h, fake_rsi)
+        change_24h = await get_change_24h(coin["id"])
+        fake_rsi = 55  # RSI подключим следующим шагом
+        probability = calculate_probability(change_24h, fake_rsi)
 
         new_signals.append({
             "id": coin["id"],
