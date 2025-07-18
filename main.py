d751a59af4e53e3ae7218a6c1a2f2e07b5548b85
Korban88 <korban88@gmail.com>
2025-07-18 14:52:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import logging
-from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, filters
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler
 from config import TELEGRAM_BOT_TOKEN
-from handlers import (
-    start_handler,
-    get_signal_handler,
-    follow_coin_handler,
-    stop_tracking_handler,
-    reset_cache_handler
-)
+from handlers import start_handler, get_signal_handler, follow_coin_handler, stop_tracking_handler, reset_cache_handler
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
@@ -19,9 +13,13 @@ def setup_application() -> Application:
     application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
 
     application.add_handler(CommandHandler("start", start_handler))
+    application.add_handler(CommandHandler("get_signal", get_signal_handler))
+    application.add_handler(CommandHandler("stop_tracking", stop_tracking_handler))
     application.add_handler(CommandHandler("reset_cache", reset_cache_handler))
-    application.add_handler(MessageHandler(filters.TEXT & filters.Regex("^Получить сигнал$"), get_signal_handler))
-    application.add_handler(MessageHandler(filters.TEXT & filters.Regex("^Остановить все отслеживания$"), stop_tracking_handler))
+
+    application.add_handler(CallbackQueryHandler(get_signal_handler, pattern="^get_signal$"))
+    application.add_handler(CallbackQueryHandler(stop_tracking_handler, pattern="^stop_tracking$"))
+    application.add_handler(CallbackQueryHandler(reset_cache_handler, pattern="^reset_cache$"))
     application.add_handler(CallbackQueryHandler(follow_coin_handler, pattern="^follow_"))
 
     return application
