85da6e6ed0ba440408b4195e6455cc05e19a6e8c
Korban88 <korban88@gmail.com>
2025-07-19 16:55:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler, MessageHandler, filters
-from config import TELEGRAM_BOT_TOKEN
+import asyncio
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    ApplicationBuilder, CommandHandler, MessageHandler,
+    ContextTypes, CallbackQueryHandler, filters
+)
 from handlers import (
-    start_handler,
-    get_signal_handler,
-    follow_coin_handler,
-    stop_tracking_handler,
-    reset_cache_handler,
-    text_message_handler
+    start_handler, text_message_handler,
+    button_callback_handler, reset_cache_handler
 )
 
-def setup_application():
-    application = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+TOKEN = "8148906065:AAEw8yAPKnhjw3AK2tsYEo-h9LVj74xJS4c"
 
-    application.add_handler(CommandHandler("start", start_handler))
-    application.add_handler(CallbackQueryHandler(follow_coin_handler, pattern="^follow_"))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_message_handler))
+keyboard = [
+    [InlineKeyboardButton("Получить сигнал", callback_data="get_signal")],
+    [InlineKeyboardButton("Сбросить кеш", callback_data="reset_cache")],
+    [InlineKeyboardButton("Остановить все отслеживания", callback_data="stop_tracking")]
+]
 
-    return application
+markup = InlineKeyboardMarkup(keyboard)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Добро пожаловать в новую жизнь, Корбан!",
+        reply_markup=markup
+    )
+
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT, text_message_handler))
+    app.add_handler(CallbackQueryHandler(button_callback_handler))
+
+    print("Бот запущен.")
+    await app.run_polling()
 
 if __name__ == "__main__":
-    app = setup_application()
-    app.run_polling()
+    asyncio.run(main())
