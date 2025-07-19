87e330298c578a2f80626627d6e896c93e1002d2
Korban88 <korban88@gmail.com>
2025-07-19 20:54:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 import asyncio
 import nest_asyncio
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler
-from config import TELEGRAM_BOT_TOKEN, OWNER_ID
+from telegram.ext import ApplicationBuilder
+from config import TELEGRAM_BOT_TOKEN
 from handlers import start_handler, button_handler
 from scheduler import schedule_daily_signal_check
 
-nest_asyncio.apply()  # Обход ошибки "event loop is already running"
-
+nest_asyncio.apply()
 
 async def main():
-    print("Бот запущен.")
+    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    app = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
-
-    # Регистрация обработчиков команд и кнопок
-    app.add_handler(CommandHandler("start", start_handler))
-    app.add_handler(CallbackQueryHandler(button_callback_handler))
+    # Добавление обработчиков
+    app.add_handler(start_handler)
+    app.add_handler(button_handler)
 
     # Планировщик ежедневного сигнала в 8:00
-    schedule_daily_signal_check(app, OWNER_ID)
+    schedule_daily_signal_check(app)
 
-    # Запуск бота
+    print("Бот запущен.")
     await app.run_polling()
-
 
 if __name__ == "__main__":
     asyncio.run(main())
