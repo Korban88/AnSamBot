5353e8858bfe6ef8d2f9ed4cc1daaf56888aa128
Korban88 <korban88@gmail.com>
2025-07-19 19:14:16 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,24 +1,10 @@
 import os
-import json
-
-INDICATORS_CACHE_FILE = "indicators_cache.json"
-SIGNALS_CACHE_FILE = "signals_cache.json"
-LAST_SIGNALS_FILE = "last_signals.json"
 
 def reset_cache():
-    # Очистка indicators_cache.json
-    if os.path.exists(INDICATORS_CACHE_FILE):
-        with open(INDICATORS_CACHE_FILE, "w") as f:
-            json.dump({}, f)
-
-    # Очистка signals_cache.json
-    if os.path.exists(SIGNALS_CACHE_FILE):
-        with open(SIGNALS_CACHE_FILE, "w") as f:
-            json.dump({}, f)
-
-    # Очистка last_signals.json
-    if os.path.exists(LAST_SIGNALS_FILE):
-        with open(LAST_SIGNALS_FILE, "w") as f:
-            json.dump([], f)
-
-    return "Кэш успешно сброшен."
+    files = [
+        "indicators_cache.json",
+        "used_symbols.json"
+    ]
+    for file in files:
+        if os.path.exists(file):
+            os.remove(file)
