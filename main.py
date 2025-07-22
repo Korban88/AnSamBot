c919565c638657703972e059036fb099f1a4fa33
Korban88 <korban88@gmail.com>
2025-07-22 11:26:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ import asyncio
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, MessageHandler, filters
 from handlers import start_handler, button_handler, message_handler
 from utils import schedule_daily_signal_check
-from config import TELEGRAM_BOT_TOKEN, OWNER_ID
+from config import TELEGRAM_BOT_TOKEN
 
 async def main():
     app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
-    app.add_handler(start_handler)               # /start
-    app.add_handler(button_handler)              # inline кнопки под сообщением
-    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), message_handler))  # reply кнопки
+    app.add_handler(start_handler)
+    app.add_handler(button_handler)
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), message_handler))
 
-    # Запланировать ежедневную отправку сигнала
     schedule_daily_signal_check(app)
 
     print("Бот запущен.")
-    await app.run_polling()
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+    await app.updater.idle()
 
 if __name__ == "__main__":
     asyncio.run(main())
